38e5971a6c83ce05dcb0ad4f931cbc3a2455d297
George204 <97558179+George204@users.noreply.github.com>
2024-07-07 00:16:26 +0200

    2x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x102x10x10

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ clock = p.time.Clock()
 running = True
 dt = 0
 
-player_pos = p.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 scr_w = screen.get_width()
 scr_h = screen.get_height()
 grid = []
 for w in range(0,23):
     line = []
     for h in range(0,12):
-        line.append((scr_w/23*w+scr_w/46,scr_h/12*h+scr_h/23))
+        line.append((scr_w/23*w+scr_w/46,scr_h/12*h+scr_h/24))
     grid.append(line)
 __import__('pprint').pprint(grid)
 while running:
@@ -23,10 +22,15 @@ while running:
             running = False
 
     screen.fill("purple")
-    
-    for line in grid:
-        for pos in line:
-            p.draw.circle(screen,(pos[0]/scr_w*255,pos[1]/scr_h*255,255),pos,scr_w/46)
+    for i in range(1,11):
+        for j in range(1,11):
+            p.draw.circle(screen,"red",grid[i][j],scr_w/46)
+    for i in range(12,22):
+        for j in range(1,11):
+            p.draw.circle(screen,"green",grid[i][j],scr_w/46)
+#    for line in grid:
+#        for pos in line:
+#            p.draw.circle(screen,(pos[0]/scr_w*255,pos[1]/scr_h*255,255),pos,scr_w/46)
 
     p.display.flip()
 
