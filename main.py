c39c935134411a65fe65b3bad318d8e4de18af7e
George204 <97558179+George204@users.noreply.github.com>
2024-07-07 22:29:46 +0200

    hit detatcion

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ screen = p.display.set_mode((1104, 576))
 clock = p.time.Clock()
 running = True
 dt = 0
-
+m_last = False
 scr_w = screen.get_width()
 scr_h = screen.get_height()
 grid = []
@@ -20,7 +20,6 @@ while running:
     for event in p.event.get():
         if event.type == p.QUIT:
             running = False
-
     screen.fill("purple")
     for i in range(1,11):
         for j in range(1,11):
@@ -28,10 +27,14 @@ while running:
     for i in range(12,22):
         for j in range(1,11):
             p.draw.circle(screen,"green",grid[i][j],scr_w/46)
-#    for line in grid:
-#        for pos in line:
-#            p.draw.circle(screen,(pos[0]/scr_w*255,pos[1]/scr_h*255,255),pos,scr_w/46)
-
+    if p.mouse.get_pressed(num_buttons=3)[0] == False:
+        m_last = False
+    if p.mouse.get_pressed(num_buttons=3)[0] == True and m_last == False:
+        m_last = True
+        m_pos = p.mouse.get_pos() 
+        m_grid_x = int(m_pos[0]/48)
+        m_grid_y = int(m_pos[1]/48)
+        print(f"{m_grid_x},{m_grid_y}")
     p.display.flip()
 
     dt = clock.tick(60) / 1000
