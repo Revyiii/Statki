3af9a1f330c612a1389b7d0ad03f878e149b788c
Revyiii <96985670+Revyiii@users.noreply.github.com>
2024-09-13 19:40:27 +0200

    menu pt2

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ screen = p.display.set_mode((1104, 576))
 clock = p.time.Clock()
 running = True
 dt = 0
-m_last = False
+
 scr_w = screen.get_width()
 scr_h = screen.get_height()
 grid = []
@@ -23,13 +23,15 @@ for w in range(0,23):
     for h in range(0,12):
         line.append((scr_w/23*w+scr_w/46,scr_h/12*h+scr_h/24))
     grid.append(line)
-def play(running):
+def play(running,losowanie):
+    m_last = False
+    
+    screen.fill("black")
     TG = [row[:] for row in tablica]
     TK = [row[:] for row in tablica]
     print(cls+"\033[?25l")
     re = [True]
-    losowanie = True
-    print("czy chesz wczytac zapis?(t/n)")
+    #print("czy chesz wczytac zapis?(t/n)")
     #if(getin()=='t'):
     if(False):
         try:
@@ -41,43 +43,43 @@ def play(running):
             print("nie ma pliku program zostanie zamknięty")
             getch()
 
-        while running:
+    while running:
 
-            if(losowanie):
-                zeruj(TG)
-                zeruj(TK)
-                lost(TG)
-                lost(TK)
-                losowanie = False
-            zapisz(TG,TK)
-            
-            for event in p.event.get():
-                if event.type == p.QUIT:
-                    running = False
-            screen.fill("purple")
-            for i in range(1,11):
-                for j in range(1,11):
-                    p.draw.circle(screen,colour[TK[i][j]],grid[i][j],scr_w/46)
-            for i in range(12,22):
-                for j in range(1,11):
-                    p.draw.circle(screen,colour[TG[i-11][j]],grid[i][j],scr_w/46)
-            if p.mouse.get_pressed(num_buttons=3)[0] == False:
-                m_last = False
-            if p.mouse.get_pressed(num_buttons=3)[0] == True and m_last == False:
-                m_last = True
-                m_pos = p.mouse.get_pos() 
-                m_grid_x = int(m_pos[0]/48)
-                m_grid_y = int(m_pos[1]/48)
-                print(f"{m_grid_x},{m_grid_y}")
-                if m_grid_x in range(1,11) and m_grid_y in range(1,11):
-                    if TK[m_grid_x][m_grid_y] == 0 or TK[m_grid_x][m_grid_y] == 5:
-                        TK[m_grid_x][m_grid_y] = 7
-                    elif TK[m_grid_x][m_grid_y] < 5 and TK[m_grid_x][m_grid_y]>0:
-                        TK[m_grid_x][m_grid_y] = 8
-                    SK(TG)
-            p.display.flip()
+        if(losowanie):
+            zeruj(TG)
+            zeruj(TK)
+            lost(TG)
+            lost(TK)
+            losowanie = False
+        zapisz(TG,TK)
+        
+        for event in p.event.get():
+            if event.type == p.QUIT:
+                running = False
+        screen.fill("purple")
+        for i in range(1,11):
+            for j in range(1,11):
+                p.draw.circle(screen,colour[TK[i][j]],grid[i][j],scr_w/46)
+        for i in range(12,22):
+            for j in range(1,11):
+                p.draw.circle(screen,colour[TG[i-11][j]],grid[i][j],scr_w/46)
+        if p.mouse.get_pressed(num_buttons=3)[0] == False:
+            m_last = False
+        if p.mouse.get_pressed(num_buttons=3)[0] == True and m_last == False:
+            m_last = True
+            m_pos = p.mouse.get_pos() 
+            m_grid_x = int(m_pos[0]/48)
+            m_grid_y = int(m_pos[1]/48)
+            print(f"{m_grid_x},{m_grid_y}")
+            if m_grid_x in range(1,11) and m_grid_y in range(1,11):
+                if TK[m_grid_x][m_grid_y] == 0 or TK[m_grid_x][m_grid_y] == 5:
+                    TK[m_grid_x][m_grid_y] = 7
+                elif TK[m_grid_x][m_grid_y] < 5 and TK[m_grid_x][m_grid_y]>0:
+                    TK[m_grid_x][m_grid_y] = 8
+                SK(TG)
+        p.display.flip()
 
-            dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 def button(screen,text):
     p.draw.rect(screen,c[2],text[2])
     screen.blit(text[1], text[2])
@@ -114,11 +116,8 @@ def menu(running):
             m_last = True
             m_pos = p.mouse.get_pos() 
             if m_pos[0] in range(button_text[0][2].left,button_text[0][2].left+button_text[0][2].width) and m_pos[1] in range(button_text[0][2].top,button_text[0][2].top+button_text[0][2].height):
-                play(running)
+                play(running,True)
            
-                
-        
-
         p.display.flip()
 
         dt = clock.tick(60) / 1000
